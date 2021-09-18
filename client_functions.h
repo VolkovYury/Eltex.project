5fcccd8c7602a451234d7a5f60a33027b0597a73
Kushchenko Maksim <AkForCorona@gmail.com>
2021-09-18 18:16:29 +0700

    added function what make order, and handle answer

--- client_functions.h
+++ client_functions.h
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include "products.pb-c.h"
 
+void orderCard(int fd, const Product *card, ProductList **list);
 int requestDatabase(int fd, ProductList **list);
+size_t findCard(const Product *card, const ProductList *database);
 
 #endif
